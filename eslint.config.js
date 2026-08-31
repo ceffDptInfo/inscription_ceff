a60daee2981fdf947c1ec323807673aa16c7b3ee
Jean-Christophe Mathez <jean-christophe.mathez@ceff.ch>
2026-08-31 13:35:26 +0200

    reconfigure action pour les lancer en manuel

--- eslint.config.js
+++ eslint.config.js
@@ -39,6 +39,7 @@ export default defineConfigWithVueTs(
         },
         rules: {
             'vue/multi-word-component-names': 'off',
+            'vue/block-lang': 'off',
             '@typescript-eslint/no-explicit-any': 'off',
             '@typescript-eslint/consistent-type-imports': [
                 'error',
